1b7d6b7a5969e83aa2590bb9857a14a1c22e2781
Yuuki Wesp <ls-micro@ya.ru>
2017-08-23 19:09:42 +0300

    Delete SpawnManger.js

--- SpawnManger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SpawnManager = (function () {
-    function SpawnManager() {
-    }
-    return SpawnManager;
-}());
-exports.SpawnManager = SpawnManager;
